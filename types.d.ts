debb04881bd6d356142e5bbd6687cbb1312f08cc
Bùi Tân Tiến <tienbt@aisolutionsjsc.com>
2023-08-02 20:01:38 +0700

    fix ts

--- types.d.ts
+++ types.d.ts
@@ -32,26 +32,45 @@ interface IHomeChartDetail {
   freq: number;
 }
 
-interface ITableCompany{
-  id: number,
-  2022: number,
-  2021: number,
-  2020: number,
+interface ITableCompany {
+  id: number;
+  2022: number;
+  2021: number;
+  2020: number;
 }
 
-interface ICompanyDetail{
-  id: number,
-  logo: any,
-  name: string,
-  description: string,
-  value1: number,
-  value2: number,
-  percent: number,
-  tags: string[],
-  dataChart: number[][],
+interface ICompanyDetail {
+  id: number;
+  logo: any;
+  name: string;
+  description: string;
+  value1: number;
+  value2: number;
+  percent: number;
+  tags: string[];
+  dataChart: number[][];
   keyStatistics: {
-    marketCap: number,
-    currentShare: number,
-    dataTable: ITableCompany[]
-  }
+    marketCap: number;
+    currentShare: number;
+    dataTable: ITableCompany[];
+  };
+}
+
+interface IPortfolio {
+  value: number;
+  profitLoss: number;
+  capitalGain: number;
+  open: number;
+}
+
+interface IPortfolioCompany {
+  id: 1;
+  logo: any;
+  name: string;
+  description: string;
+  investment: number;
+  profitLoss: number;
+  profitLossPercent: number;
+  allocate: number;
+  allocatePercent: number;
 }
